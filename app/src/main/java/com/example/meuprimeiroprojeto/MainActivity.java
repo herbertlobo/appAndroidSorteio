388a7d2e59fbaf0d2d84e96eaf145d8e4ae72bf3
herbertlobo <herbertlobo@outlook.com>
2021-02-17 18:50:49 -0300

    primeiro apps

--- app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.java
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.meuprimeiroprojeto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sortearNumero(View view){
+
+        TextView texto = findViewById(R.id.txtResultado);
+        texto.setText("Númro: "+(new Random().nextInt(11)));
+    }
+
+
+
 }
